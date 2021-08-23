01a54b7417e343b21a276922ef756127fdf7045e
gorushkin <gorushkin@users.noreply.github.com>
2021-08-23 19:42:25 +0000

    Deploying to gh-pages from @ gorushkin/geoconect@29ddc30792dd60ecae0de4b91a87b29b973cd840 🚀

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
 "use strict";
 
-console.log('start');
+var elements = {
+  menu: document.querySelector('.js__menu'),
+  btn: document.querySelector('.js__btn')
+};
+var state = {
+  isMenuOpen: false
+};
+
+var render = function render() {
+  var menuStyle = state.isMenuOpen ? 'block' : 'none';
+  elements.menu.style.display = menuStyle;
+  var btnStateClass = "header__btn js__btn header__btn--".concat(state.isMenuOpen ? 'close' : 'open');
+  console.log('btnStateClass: ', btnStateClass); // elements.btn.setAttribute.class = btnStateClass;
+
+  elements.btn.className = btnStateClass;
+};
+
+var toggleState = function toggleState() {
+  state.isMenuOpen = !state.isMenuOpen;
+  render();
+};
+
+elements.btn.addEventListener('click', toggleState);
 //# sourceMappingURL=main.js.map
