29ddc30792dd60ecae0de4b91a87b29b973cd840
gorushkin <gorushkin@gmail.com>
2021-08-23 22:41:40 +0300

    header mobile

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -1,1 +1,24 @@
-console.log('start');
+const elements = {
+  menu: document.querySelector('.js__menu'),
+  btn: document.querySelector('.js__btn'),
+};
+
+const state = {
+  isMenuOpen: false,
+};
+
+const render = () => {
+  const menuStyle = state.isMenuOpen ? 'block' : 'none';
+  elements.menu.style.display = menuStyle;
+  const btnStateClass = `header__btn js__btn header__btn--${state.isMenuOpen ? 'close' : 'open'}`;
+  console.log('btnStateClass: ', btnStateClass);
+  // elements.btn.setAttribute.class = btnStateClass;
+  elements.btn.className = btnStateClass;
+};
+
+const toggleState = () => {
+  state.isMenuOpen = !state.isMenuOpen;
+  render();
+};
+
+elements.btn.addEventListener('click', toggleState);
