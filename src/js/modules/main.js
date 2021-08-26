183579101c33f88b7bead2b964a2cbf6a92c55c0
gorushkin <gorushkin@gmail.com>
2021-08-26 22:36:54 +0300

    header, promo mobile are ready

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -1,6 +1,8 @@
 const elements = {
   menu: document.querySelector('.js__menu'),
   btn: document.querySelector('.js__btn'),
+  header: document.querySelector('.js__header'),
+  body: document.body,
 };
 
 const state = {
@@ -9,11 +11,13 @@ const state = {
 
 const render = () => {
   const menuStyle = state.isMenuOpen ? 'block' : 'none';
-  elements.menu.style.display = menuStyle;
+  const headerClasse = `header js__header header--${state.isMenuOpen ? 'open' : 'closed'}`;
   const btnStateClass = `header__btn js__btn header__btn--${state.isMenuOpen ? 'close' : 'open'}`;
-  console.log('btnStateClass: ', btnStateClass);
-  // elements.btn.setAttribute.class = btnStateClass;
+
+  elements.menu.style.display = menuStyle;
   elements.btn.className = btnStateClass;
+  elements.header.className = headerClasse;
+  elements.body.style.overflow = state.isMenuOpen ? 'hidden' : '';
 };
 
 const toggleState = () => {
