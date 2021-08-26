0560555cf920f9493ca4b01f78f3ba6c54402e68
gorushkin <gorushkin@users.noreply.github.com>
2021-08-26 19:37:58 +0000

    Deploying to gh-pages from @ gorushkin/geoconect@183579101c33f88b7bead2b964a2cbf6a92c55c0 🚀

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 var elements = {
   menu: document.querySelector('.js__menu'),
-  btn: document.querySelector('.js__btn')
+  btn: document.querySelector('.js__btn'),
+  header: document.querySelector('.js__header'),
+  body: document.body
 };
 var state = {
   isMenuOpen: false
@@ -10,11 +12,12 @@ var state = {
 
 var render = function render() {
   var menuStyle = state.isMenuOpen ? 'block' : 'none';
-  elements.menu.style.display = menuStyle;
+  var headerClasse = "header js__header header--".concat(state.isMenuOpen ? 'open' : 'closed');
   var btnStateClass = "header__btn js__btn header__btn--".concat(state.isMenuOpen ? 'close' : 'open');
-  console.log('btnStateClass: ', btnStateClass); // elements.btn.setAttribute.class = btnStateClass;
-
+  elements.menu.style.display = menuStyle;
   elements.btn.className = btnStateClass;
+  elements.header.className = headerClasse;
+  elements.body.style.overflow = state.isMenuOpen ? 'hidden' : '';
 };
 
 var toggleState = function toggleState() {
