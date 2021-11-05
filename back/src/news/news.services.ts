0943f687679c9c4217535e55bb226e6e54d0c53b
gorushkin <gorushkin@gmail.com>
2021-11-05 18:37:55 +0300

    доделал fileHandler

--- back/src/news/news.services.ts
+++ back/src/news/news.services.ts
@@ -1,56 +1,42 @@
-import { UploadedFile, FileArray } from 'express-fileupload';
-// import { v4 as uuidv4 } from 'uuid';
+import { FileArray } from 'express-fileupload';
 import path from 'path';
 const dirname = path.join(path.resolve());
-import { promises as fs } from 'fs';
+import { v4 as uuid } from 'uuid';
 
-const getPath = (filename: string) => {
+const getFileExtension = (filename: string) => filename.split('.').pop();
+
+const getFilename = (inputFilename: string) => {
+  const fileExtension = getFileExtension(inputFilename);
+  const filname = uuid();
+  return `${filname}.${fileExtension}`;
+};
+
+const getPath = (inputFilename: string) => {
+  const filename = getFilename(inputFilename);
   return path.join(dirname, 'images', filename);
 };
 
-// export const fileHandler = async (file: UploadedFile) => {
-//   const date = new Date();
-//   const currentDate = `${date.getFullYear()}${date.getMonth()}${date.getMonth()}${date.getDay()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
-//   const uploadPath = path.join(dirname, 'images', currentDate);
-//   try {
-//     await fs.writeFile(uploadPath, file.data);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export const fileHandler = async (files: UploadedFile[]) => {
-//   console.log(dirname);
-//   console.log(files);
-//   console.log(process.cwd());
-//   // console.log(file.name);
-//   // files.forEach((element: UploadedFile) => {
-//   //   console.log(element.name);
-//   // });
-//   // console.log('fileHandler');
-//   const date = new Date();
-//   const currentDate = `${date.getFullYear()}${date.getMonth()}${date.getMonth()}${date.getDay()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}`;
-//   const uploadPath = path.join(dirname, currentDate);
-//   console.log('uploadPath: ', uploadPath);
-//   try {
-//     files.forEach((file) => {
-//       console.log(file);
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-//   // const filename = date.getDate();
-// };
-
 export const fileHandler = async (data: FileArray) => {
-  console.log(data);
-  const date = new Date();
-  const files = Object.values(data).map((file) => {
-    if (Array.isArray(file)) {
-      return file.map((file) => file.mv(getPath(file.name)));
-    }
-    return file.mv(getPath(file.name));
-  });
+  const files = Object.entries(data)
+    .map(([key, value]) =>
+      Array.isArray(value)
+        ? value.map((item) => ({ ...item, formName: key }))
+        : { ...value, formName: key }
+    )
+    .flat(1);
 
-  console.log('files: ', files);
+  const result = files
+    .map((file) => {
+      const path = getPath(file.name);
+      try {
+        file.mv(path);
+        return path;
+      } catch (error) {
+        console.log('error: ', error);
+        return '';
+      }
+    })
+    .filter((file) => file);
+
+  return result;
 };
