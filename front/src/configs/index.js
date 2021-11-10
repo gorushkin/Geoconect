9b4e5515adce14e514f2d138821d4df3813fa691
gorushkin <gorushkin@gmail.com>
2021-11-10 21:48:25 +0300

    правка конфигов

--- front/src/configs/index.js
+++ front/src/configs/index.js
@@ -1,6 +1,7 @@
+import dev from './dev';
 import local from './local';
 import prod from './prod';
 
-const serverTypeMapping = { local, prod };
+const serverTypeMapping = { local, prod, dev };
 
 export default serverTypeMapping[process.env.NEXT_PUBLIC_TYPE] || serverTypeMapping.local;
