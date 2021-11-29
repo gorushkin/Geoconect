31c750d3e6d981f50fd2c2002e70b0bf61843140
gorushkin <gorushkin@gmail.com>
2021-11-29 08:46:42 +0300

    настроил работу с токенами и защитил роуты

--- back/src/helpers/errorHanlder.ts
+++ back/src/helpers/errorHanlder.ts
@@ -1,8 +1,17 @@
 import { Request, Response, ErrorRequestHandler, NextFunction } from 'express';
 
+export class CustomError extends Error {
+  status: string;
+
+  constructor(error: string, status: string) {
+    super(error);
+    this.status = status;
+  }
+}
+
 export const ErrorHandler: ErrorRequestHandler = (error, _req, res, next) => {
   const message = error instanceof Error ? error.message : error;
-  console.log('message: ', message);
+  console.log('ErrorHandler: ', message);
   res.status(500).send(message);
   next();
 };
--- back/src/resources/auth/auth.services.ts
+++ back/src/resources/auth/auth.services.ts
@@ -2,21 +2,19 @@ import { Request, Response, NextFunction } from 'express';
 import User from '../users/users.model';
 import jwt from 'jsonwebtoken';
 import { CONFIG } from '../../helpers/config';
+import { CustomError } from '../../helpers/errorHanlder';
 
 export const createToken = (user: User, password: string) => {
   const compareResults = user.verifyPassword(password);
   if (!compareResults) throw Error('Paswword is not corrent');
-  const token = jwt.sign({ name: user.name }, CONFIG.SECRET, {
-    expiresIn: '4h',
-  });
-
+  const token = jwt.sign({ name: user.name }, CONFIG.SECRET);
   return token;
 };
 
 export const authMiddleware = (req: Request, _res: Response, next: NextFunction) => {
-  const {authorization} = req.headers;
-  if (authorization) {
-    console.log(authorization);
-  }
+  const { authorization } = req.headers;
+  if (!authorization) throw new CustomError('UNAUTHORIZED', '401');
+  const data = jwt.verify(authorization, CONFIG.SECRET);
+  if (typeof data !== 'string') console.log(data['name']);
   next();
 };
